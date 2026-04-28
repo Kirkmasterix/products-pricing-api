10fe3e3ad7ba93a917c204f4f407338dadf650ac
Kirkmasterix <denmanuel888@gmail.com>
2026-04-28 18:52:26 +0200

    Add Product Mapper

--- src/main/java/com/mango/products/mapper/ProductMapper.java
+++ src/main/java/com/mango/products/mapper/ProductMapper.java
@@ -1,2 +1,23 @@
-package com.mango.products.mapper;public class ProductMapper {
+package com.mango.products.mapper;
+
+import com.mango.products.domain.Product;
+import com.mango.products.dto.CreateProductRequest;
+import com.mango.products.dto.ProductResponse;
+
+public class ProductMapper {
+
+    public static Product toEntity(CreateProductRequest request) {
+        Product product = new Product();
+        product.setName(request.name());
+        product.setDescription(request.description());
+        return product;
+    }
+
+    public static ProductResponse toResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription()
+        );
+    }
 }
