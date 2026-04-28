56d88443252a2707eba5eb627f203663a9f8f19d
Kirkmasterix <denmanuel888@gmail.com>
2026-04-28 20:10:00 +0200

    Add methods to PriceRepo

--- src/main/java/com/mango/products/repository/PriceRepository.java
+++ src/main/java/com/mango/products/repository/PriceRepository.java
@@ -3,5 +3,16 @@ package com.mango.products.repository;
 import com.mango.products.domain.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PriceRepository extends JpaRepository<Price, Long> {
+    List<Price> findByProductId(Long productId);
+
+    // precio activo en una fecha
+    List<Price> findByProductIdAndInitDateLessThanEqualAndEndDateGreaterThanEqual(
+            Long productId,
+            LocalDate date1,
+            LocalDate date2
+    );
 }
