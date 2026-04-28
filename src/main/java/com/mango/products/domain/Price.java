1ceea51517445b5c02a55f2fec9a35f376892bc0
Kirkmasterix <denmanuel888@gmail.com>
2026-04-28 18:14:16 +0200

    Add entities for Price and Product

--- src/main/java/com/mango/products/domain/Price.java
+++ src/main/java/com/mango/products/domain/Price.java
@@ -1,2 +1,30 @@
-package com.mango.products.domain;public class Price {
+package com.mango.products.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prices")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double value;
+
+    private LocalDate initDate;
+
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
--- src/main/java/com/mango/products/domain/Product.java
+++ src/main/java/com/mango/products/domain/Product.java
@@ -1,2 +1,26 @@
-package com.mango.products.domain;public class Product {
+package com.mango.products.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Price> prices;
+
 }
