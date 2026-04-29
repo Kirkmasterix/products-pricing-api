d135bba8c29fca40c8f1897bcbd62364d42f21f4
Kirkmasterix <denmanuel888@gmail.com>
2026-04-29 17:13:29 +0200

    Add Global Exception Handler

---
+++ src/main/java/com/mango/products/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.mango.products.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(PriceNotFoundException.class)
+    public ResponseEntity<String> handlePriceNotFound(PriceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPriceDateException.class)
+    public ResponseEntity<String> handleInvalidDate(InvalidPriceDateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+}
