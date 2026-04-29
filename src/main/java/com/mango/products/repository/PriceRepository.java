57eb630fd076ea45384c659c3c07f533ca10c236
Kirkmasterix <denmanuel888@gmail.com>
2026-04-29 09:56:00 +0200

    Add query to findActivePrice and findFirst product by id and end date to price repo

--- src/main/java/com/mango/products/repository/PriceRepository.java
+++ src/main/java/com/mango/products/repository/PriceRepository.java
@@ -2,17 +2,23 @@ package com.mango.products.repository;
 
 import com.mango.products.domain.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface PriceRepository extends JpaRepository<Price, Long> {
+
     List<Price> findByProductId(Long productId);
 
-    // precio activo en una fecha
-    List<Price> findByProductIdAndInitDateLessThanEqualAndEndDateGreaterThanEqual(
-            Long productId,
-            LocalDate date1,
-            LocalDate date2
-    );
+    @Query("""
+      SELECT p FROM Price p
+      WHERE p.product.id = :productId
+      AND p.initDate <= :date
+      AND (p.endDate IS NULL OR p.endDate >= :date)
+    """)
+    Optional<Price> findActivePrice(Long productId, LocalDate date);
+
+    Optional<Price> findFirstByProductIdAndEndDateIsNull(Long productId);
 }
