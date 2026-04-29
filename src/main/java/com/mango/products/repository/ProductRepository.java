4c5b6dd5eddce56d9f75c72dd2580e8748973d8c
Kirkmasterix <denmanuel888@gmail.com>
2026-04-29 11:10:52 +0200

    Add findByIdWithPrices in Product repo

--- src/main/java/com/mango/products/repository/ProductRepository.java
+++ src/main/java/com/mango/products/repository/ProductRepository.java
@@ -2,6 +2,18 @@ package com.mango.products.repository;
 
 import com.mango.products.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("""
+     SELECT p FROM Product p
+     LEFT JOIN FETCH p.prices pr
+     WHERE p.id = :id
+     ORDER BY pr.initDate ASC
+    """)
+    Optional<Product> findByIdWithPrices(@Param("id") Long id);
 }
